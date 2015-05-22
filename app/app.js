7966349d70a235eb547f3ac280875f1a215b6cb2
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-05-22 20:00:39 +0100

    geolocation added

--- app/app.js
+++ app/app.js
@@ -1,24 +1,20 @@
 var express = require('express')
-  , passport = require('passport')
-  , util = require('util')
-  , FacebookStrategy = require('passport-facebook').Strategy
-  , logger = require('morgan')
-  , session = require('express-session')
-  , bodyParser = require("body-parser")
-  , cookieParser = require("cookie-parser")
-  , methodOverride = require('method-override');
+var passport = require('passport')
+var util = require('util')
+var FacebookStrategy = require('passport-facebook').Strategy
+var logger = require('morgan')
+var session = require('express-session')
+var bodyParser = require("body-parser")
+var cookieParser = require("cookie-parser")
+var methodOverride = require('method-override');
+var port = process.env.PORT || 3000
+var io = require('socket.io')(http);
+var http = require('http').Server(app);
 
-var FACEBOOK_APP_ID = "--insert-facebook-app-id-here--"
-var FACEBOOK_APP_SECRET = "--insert-facebook-app-secret-here--";
+var FACEBOOK_APP_ID = 
+var FACEBOOK_APP_SECRET = 
 
 
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Facebook profile is serialized
-//   and deserialized.
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -28,10 +24,6 @@ passport.deserializeUser(function(obj, done) {
 });
 
 
-// Use the FacebookStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Facebook
-//   profile), and invoke a callback with a user object.
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
@@ -55,7 +47,6 @@ passport.use(new FacebookStrategy({
 
 var app = express();
 
-// configure Express
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(logger());
@@ -63,8 +54,7 @@ var app = express();
   app.use(bodyParser());
   app.use(methodOverride());
   app.use(session({ secret: 'keyboard cat' }));
-  // Initialize Passport!  Also use passport.session() middleware, to support
-  // persistent login sessions (recommended).
+
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(express.static(__dirname + '/public'));
@@ -82,23 +72,13 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-// GET /auth/facebook
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Facebook authentication will involve
-//   redirecting the user to facebook.com.  After authorization, Facebook will
-//   redirect the user back to this application at /auth/facebook/callback
+
 app.get('/auth/facebook',
   passport.authenticate('facebook'),
   function(req, res){
-    // The request will be redirected to Facebook for authentication, so this
-    // function will not be called.
   });
 
-// GET /auth/facebook/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
+
 app.get('/auth/facebook/callback', 
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
@@ -110,14 +90,27 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-app.listen(3000);
+app.get('/mapjs', function(req, res){
+  res.sendFile(__dirname + '/public/map.js');
+});
 
+io.on('connection', function(socket){
+    console.log('a user connected');
+  
+    socket.on('marker', function(data) {
+      data.socketId = socket.id;
+      
+      markers[socket.id] = data;
 
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
+  
+  console.log('marker latitude: ' + data.lat + ', marker longitude:' + data.lng);
+    socket.broadcast.emit('show-marker', data);
+    });
+
+});
+
+app.listen(port);
+
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
